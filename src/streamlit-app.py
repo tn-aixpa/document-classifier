59fe303673c1048bc24f5a8694b33f1a9605ddcf
Nawaz Khurshid <nawaz1981@gmail.com>
2025-01-13 13:35:03 +0100

    Update streamlit-app.py

--- src/streamlit-app.py
+++ src/streamlit-app.py
@@ -8,23 +8,6 @@ import digitalhub as dh
 import os
 import streamlit
 
-global correspondence
-
-@st.cache_data
-def load_correspondence():
-    correspondence = pd.read_csv("src/correspondence.csv")
-
-def answer(ID_tassonomia):
-    """
-    Given the label(s), retrieves from the taxonomy the rest of the information about the prediction.
-    :param ID_tassonomia: int, list - the ID(s) of the tassonomia.
-    :return: List of tuples with (azione, campi, descrizione_codice_campo, macroambiti, descrizione_codice_macro).
-    """
-    dID, c, cd, m, md = correspondence.loc[
-        correspondence.ID_tassonomia == ID_tassonomia, ['azione', 'campi', 'descrizione_codice_campo', 'macroambiti',
-                                                        'descrizione_codice_macro']].values[0]
-    return dID, c, cd, m, md
-    
 def annotate(text, nl):
     body = {"inference_input": { "text": text, "k": nl} }
     preds = requests.post(f"http://{service_url}/", json=body).json()
@@ -32,6 +15,7 @@ def annotate(text, nl):
 
 
 st.title('Document classifier')
+correspondence = pd.read_csv("src/correspondence.csv")
 service_url = os.environ.get("SERVICE_URL", "")
 
 if service_url == None or service_url == "":
@@ -45,6 +29,7 @@ if st.button("Annota"):
     #st.text(pred['results'])
     result = ''
     for el in pred['results']:
-        dID, c, cd, m, md = answer(el)
+        dID, c, cd, m, md = correspondence.loc[
+        correspondence.ID_tassonomia == ID_tassonomia, ['azione', 'campi', 'descrizione_codice_campo', 'macroambiti', descrizione_codice_macro']].values[0]
         result = result + f'ID tassonomia di azione: {dID}' + '\n'
     st.text(result)
